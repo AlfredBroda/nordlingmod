8a9bb0b48d63ae614340314b5682bd5c94a69f79
Owl-Code <Paul.einwich@gmail.com>
2018-05-28 12:38:10 -0700

    bee and hive test and fix - hives here fine still touched up - bees are fixed : stuck in animation - added next incase bees are wanted randomly in world

--- nordlingmod_server.lua
+++ nordlingmod_server.lua
@@ -7,7 +7,6 @@ local log = radiant.log.create_logger('Nordling Mod')
 function nordlingmod:_on_required_loaded()
   local BeeTrapper = require('jobs.trapper')
   local BeeTrappingGrounds = require('components.trapping.trapping_grounds_component')
-  local BeeHiveTrap = require('components.trapping.bee_hive_component')
   log:always("Bees Loaded")
 
 end
