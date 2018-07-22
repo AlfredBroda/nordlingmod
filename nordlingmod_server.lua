7d71d679751b8bdbbf7f6a22dd62e4ef63ae5368
Thahat <39244853+Thahat@users.noreply.github.com>
2018-07-22 14:03:23 +0200

    remove bees so shit works again
    i kept backup files on my desktop

--- nordlingmod_server.lua
+++ nordlingmod_server.lua
@@ -3,14 +3,6 @@ nordlingmod = {
 
 local log = radiant.log.create_logger('Nordling Mod Server')
 
-
-function nordlingmod:_on_required_loaded()
-  local BeeTrapper = require('jobs.trapper.trapper')
-  local BeeTrappingGrounds = require('components.trapping.trapping_grounds_component')
-  log:always("Bees Loaded")
-
-end
-
 radiant.events.listen_once(radiant,'radiant:required_loaded', nordlingmod, nordlingmod._on_required_loaded)
 
 return nordlingmod
