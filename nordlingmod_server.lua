6fdcf2a3854ce68e3fa8407f7de49a58e53f1caa
Thahat <39244853+Thahat@users.noreply.github.com>
2018-07-22 14:31:29 +0200

    remove error on startup
    by removing server file, was used for bees

--- nordlingmod_server.lua
+++
@@ -1,8 +0,0 @@
-nordlingmod = {
-}
-
-local log = radiant.log.create_logger('Nordling Mod Server')
-
-radiant.events.listen_once(radiant,'radiant:required_loaded', nordlingmod, nordlingmod._on_required_loaded)
-
-return nordlingmod
