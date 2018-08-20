3a385ed4257baa9449df3cd8235849901a95f3d3
Thahat <39244853+Thahat@users.noreply.github.com>
2018-08-20 22:18:54 +0200

    questfix

--- data/gm/campaigns/orc_war/arcs/trigger/old_khan/encounters/make_orc_npcs_friendly.lua
+++
@@ -1,7 +0,0 @@
-local MakeOrcNPCsFriendly = class()
-
-function MakeOrcNPCsFriendly:start(ctx, data)
-   stonehearth.player:set_amenity(ctx.player_id, 'orc_npcs', 'friendly')
-end
-
-return MakeOrcNPCsFriendly
