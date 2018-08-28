3d9eea2adec19f68b7c78a07c513d59fd697248f
Thahat <39244853+Thahat@users.noreply.github.com>
2018-08-28 20:25:24 +0200

    brunomagic

---
+++ entities/consumables/scripts/unlock_crop.lua
@@ -0,0 +1,21 @@
+
+local UnlockCrop = class()
+
+function UnlockCrop.use(consumable, consumable_data, player_id, target_entity)
+    local kingdom_alias = stonehearth.player:get_kingdom(player_id)
+    if kingdom == "nordlings:kingdom:nordlings" then
+        local worker_job = stonehearth.job:get_job_info(player_id, "stonehearth:jobs:worker")
+        if worker_job then  
+            worker_job:manually_unlock_crop(consumable_data.crop)
+        end
+    else
+        local farmer_job = stonehearth.job:get_job_info(player_id, "stonehearth:jobs:farmer")
+        if farmer_job then  
+            farmer_job:manually_unlock_crop(consumable_data.crop)
+        end
+    end
+    return true
+end
+end
+
+return UnlockCrop
